cf7783c49a7b9dc081b36939853c8c72f1d8bff3
me <you@example.com>
2026-06-07 23:33:28 +0200

    added pre-commit hooks and fixed commit blockers

--- lib/stack/appCluster.ts
+++ lib/stack/appCluster.ts
@@ -1,6 +1,5 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-import * as ec2 from 'aws-cdk-lib/aws-ec2';
 import { EncryptionStack } from './kms';
 import { RdsStack } from './rds';
 import { SagemakerStack } from './sagemaker';
